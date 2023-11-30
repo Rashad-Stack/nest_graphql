3a4f16f874d5fd12b50cd29e0b4448041c3585b3
Rashad-Stack <tex.eng.rashed@gmail.com>
2023-11-30 17:05:37 +0600

    Add mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Lesson } from "./lesson/lesson.entity";
 import { LessonModule } from "./lesson/lesson.module";
 
 @Module({
@@ -10,6 +12,13 @@ import { LessonModule } from "./lesson/lesson.module";
       driver: ApolloDriver,
     }),
     LessonModule,
+    TypeOrmModule.forRoot({
+      type: "mongodb",
+      url: "mongodb://localhost:root:root:27017/school",
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson],
+    }),
   ],
   controllers: [],
   providers: [],
