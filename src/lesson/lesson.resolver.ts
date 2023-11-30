bcf79c1cb4a48fd40d35523481ccc51be10f92be
Rashad-Stack <tex.eng.rashed@gmail.com>
2023-11-30 16:10:12 +0600

    Add graphql

---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,15 @@
+import { Query, Resolver } from "@nestjs/graphql";
+import { LessonType } from "./lesson.type";
+
+@Resolver(LessonType)
+export class LessonResolver {
+  @Query(() => LessonType)
+  Lesson() {
+    return {
+      id: "sdfsdf",
+      name: "Physics Class",
+      startDate: new Date().toISOString(),
+      endDate: new Date().toISOString(),
+    };
+  }
+}
