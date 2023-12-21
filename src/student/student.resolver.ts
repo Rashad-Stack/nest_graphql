0f74101120d99a1b7b644acab75597f3c62f7050
Rashad-Stack <tex.eng.rashed@gmail.com>
2023-12-21 07:50:39 +0600

    Add createStudent mutation and getStudent and getStudents queries

--- src/student/student.resolver.ts
+++ src/student/student.resolver.ts
@@ -1,4 +1,24 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
+import { StudentInputDto } from "./create-student.input.dto";
+import { StudentService } from "./student.service";
+import { StudentType } from "./student.type";
 
 @Resolver()
-export class StudentResolver {}
+export class StudentResolver {
+  constructor(private studentService: StudentService) {}
+
+  @Query(() => StudentType)
+  Student(@Args("id") id: string) {
+    return this.studentService.getStudent(id);
+  }
+
+  @Query(() => [StudentType])
+  Students() {
+    return this.studentService.getStudents();
+  }
+
+  @Mutation(() => StudentType)
+  createStudent(@Args("createStudentDto") createStudentDto: StudentInputDto) {
+    return this.studentService.createStudent(createStudentDto);
+  }
+}
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { v4 as uuid } from "uuid";
+import { StudentInputDto } from "./create-student.input.dto";
+import { Student } from "./student.entity";
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  constructor(
+    @InjectRepository(Student) private studentRepository: Repository<Student>,
+  ) {}
+
+  async getStudent(id: string): Promise<Student> {
+    return this.studentRepository.findOneBy({ id });
+  }
+
+  async getStudents(): Promise<Student[]> {
+    return this.studentRepository.find();
+  }
+
+  async createStudent(createStudentDto: StudentInputDto): Promise<Student> {
+    const { firstName, lastName } = createStudentDto;
+
+    const student = this.studentRepository.create({
+      id: uuid(),
+      firstName,
+      lastName,
+    });
+    return this.studentRepository.save(student);
+  }
+}
